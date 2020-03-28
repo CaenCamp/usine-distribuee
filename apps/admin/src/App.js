c72add7e690f553ebec5db42977261c5975e83b6
Florian FERBACH <florian@marmelab.com>
2020-03-28 11:26:46 +0100

    Basic react-admin bootstrap

--- apps/admin/src/App.js
+++ apps/admin/src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Admin } from "react-admin";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import authProvider from "./authProvider";
+import dataProvider from "./dataProvider";
+
+const App = () => (
+  <Admin
+    authProvider={authProvider}
+    dataProvider={dataProvider}
+  />
+);
 
 export default App;
