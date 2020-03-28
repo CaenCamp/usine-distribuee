b71f459b93a83d865600c30cb6cc442b1f7baacc
Kmaschta <kevin@marmelab.com>
2020-03-28 15:47:25 +0100

    Remove hero + fix a validation

--- apps/api/src/front.js
+++ apps/api/src/front.js
@@ -34,12 +34,17 @@ const parseRequest = ({ mask_small_size_quantity, mask_large_size_quantity, fore
 const validate = ({ mask_small_size_quantity, mask_large_size_quantity }) => {
     const errors = {};
 
+    if (!mask_small_size_quantity && !mask_large_size_quantity) {
+        errors.mask_small_size_quantity = 'La commande doit comporter au moins 1 masque pour être valide';
+        errors.mask_large_size_quantity = 'La commande doit comporter au moins 1 masque pour être valide';
+    }
+
     if (mask_small_size_quantity && mask_small_size_quantity < 5) {
         errors.mask_small_size_quantity = 'La commande doit comporter au moins 5 masques pour être valide';
     }
 
     if (mask_large_size_quantity && mask_large_size_quantity < 5) {
-        errors.mask_small_size_quantity = 'La commande doit comporter au moins 5 masques pour être valide';
+        errors.mask_large_size_quantity = 'La commande doit comporter au moins 5 masques pour être valide';
     }
 
     return errors;
