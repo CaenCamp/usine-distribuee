05110cf2b80f714e87110959734e90c6ed989809
Alexis Janvier <contact@alexisjanvier.net>
2020-03-29 23:19:16 +0200

    Fix ACL

--- apps/api/src/request/authorization/index.js
+++ apps/api/src/request/authorization/index.js
@@ -2,14 +2,21 @@ const get = require('lodash.get');
 
 const permissions = require('./permissions');
 
-const isFunction = value => value && (Object.prototype.toString.call(value) === "[object Function]" || "function" === typeof value || value instanceof Function);
+const isFunction = value =>
+    value &&
+    (Object.prototype.toString.call(value) === "[object Function]" ||
+        "function" === typeof value ||
+        value instanceof Function);
 
 const isAuthorized = (user, from, to) => {
-    const validator = get(permissions, `${user.role}.${from.status}.${to.status}`);
-    if(!validator){
+    const validator = get(
+        permissions,
+        `${user.role}.${from.status}.${to.status}`
+    );
+    if (!validator) {
         return false;
     }
-    if(isFunction(validator)){
+    if (isFunction(validator)) {
         return validator(user, from, to);
     }
     return !!validator;
