854c5def38fbfe9258e10e4d2970146fbe3492c1
Florian FERBACH <florian@marmelab.com>
2020-03-29 19:59:19 +0200

    Send tracking mail when request sent

--- apps/api/src/front.js
+++ apps/api/src/front.js
@@ -1,29 +1,30 @@
-const fs = require('fs');
-const path = require('path');
-const Koa = require('koa');
-const Router = require('koa-router')
-const views = require('koa-views');
-const signale = require('signale');
+const fs = require("fs");
+const path = require("path");
+const Koa = require("koa");
+const Router = require("koa-router");
+const views = require("koa-views");
+const signale = require("signale");
 
-const dbMiddleware = require('./dbMiddleware');
-const { insertOne, getOne } = require('./request/repository');
+const dbMiddleware = require("./dbMiddleware");
+const { insertOne, getOne } = require("./request/repository");
+const { sendRequestConfirmation } = require("./toolbox/mailjet");
 
 const app = new Koa();
 const router = new Router();
 
 app.use(dbMiddleware);
 
-const { mtime } = fs.statSync(path.resolve(__dirname, './views/index.ejs'));
+const { mtime } = fs.statSync(path.resolve(__dirname, "./views/index.ejs"));
 
-const initContextState = (ctx) => {
+const initContextState = ctx => {
     ctx.state.errors = {};
     ctx.state.internalError = false;
     ctx.state.request = {};
     ctx.state.success = false;
-}
+};
 
-const renderCachedHomepage = async (ctx) => {
-    ctx.set('ETag', `"${mtime.getTime().toString()}"`);
+const renderCachedHomepage = async ctx => {
+    ctx.set("ETag", `"${mtime.getTime().toString()}"`);
 
     if (ctx.fresh) {
         ctx.status = 304;
@@ -31,52 +32,75 @@ const renderCachedHomepage = async (ctx) => {
     }
 
     initContextState(ctx);
-    return ctx.render('index.ejs');
+    return ctx.render("index.ejs");
 };
 
-router.get('/', renderCachedHomepage);
+router.get("/", renderCachedHomepage);
 
-const parseRequest = ({ mask_small_size_quantity, mask_large_size_quantity, forecast_quantity, forecast_days, ...body }) => ({
+const parseRequest = ({
+    mask_small_size_quantity,
+    mask_large_size_quantity,
+    forecast_quantity,
+    forecast_days,
+    ...body
+}) => ({
     ...body,
     mask_small_size_quantity: parseInt(mask_small_size_quantity, 10),
     mask_large_size_quantity: parseInt(mask_large_size_quantity, 10),
     forecast_quantity: parseInt(forecast_quantity, 10),
-    forecast_days: parseInt(forecast_days, 10),
+    forecast_days: parseInt(forecast_days, 10)
 });
 
-const validate = ({ requester_type, requester_other_type, mask_small_size_quantity, mask_large_size_quantity }) => {
+const validate = ({
+    requester_type,
+    requester_other_type,
+    mask_small_size_quantity,
+    mask_large_size_quantity
+}) => {
     const errors = {};
 
     if (!requester_type) {
-        errors.requester_type = 'Veuillez choisir un type de numéro professionnel de santé';
+        errors.requester_type =
+            "Veuillez choisir un type de numéro professionnel de santé";
     }
 
-    if (requester_type === 'other' && (!requester_other_type && requester_other_type.trim() === '')) {
-        errors.requester_type = 'Veuillez préciser le type de numéro de professionnel de santé';
+    if (
+        requester_type === "other" &&
+        !requester_other_type &&
+        requester_other_type.trim() === ""
+    ) {
+        errors.requester_type =
+            "Veuillez préciser le type de numéro de professionnel de santé";
     }
 
     if (!mask_small_size_quantity && !mask_large_size_quantity) {
-        errors.mask_small_size_quantity = 'La demande doit comporter au moins 5 masques pour être valide';
-        errors.mask_large_size_quantity = 'La demande doit comporter au moins 5 masques pour être valide';
+        errors.mask_small_size_quantity =
+            "La commande doit comporter au moins 5 masques pour être valide";
+        errors.mask_large_size_quantity =
+            "La commande doit comporter au moins 5 masques pour être valide";
     }
 
     if (mask_small_size_quantity && mask_small_size_quantity < 5) {
-        errors.mask_small_size_quantity = 'La demande doit comporter au moins 5 masques pour être valide';
+        errors.mask_small_size_quantity =
+            "La commande doit comporter au moins 5 masques pour être valide";
     }
 
     if (mask_large_size_quantity && mask_large_size_quantity < 5) {
-        errors.mask_large_size_quantity = 'La demande doit comporter au moins 5 masques pour être valide';
+        errors.mask_large_size_quantity =
+            "La commande doit comporter au moins 5 masques pour être valide";
     }
 
     if (mask_small_size_quantity + mask_large_size_quantity > 150) {
-        errors.mask_small_size_quantity = 'La demande doit comporter au maximum 150 masques pour être valide';
-        errors.mask_large_size_quantity = 'La demande doit comporter au maximum 150 masques pour être valide';
+        errors.mask_small_size_quantity =
+            "La commande doit comporter au maximum 150 masques pour être valide";
+        errors.mask_large_size_quantity =
+            "La commande doit comporter au maximum 150 masques pour être valide";
     }
 
     return errors;
-}
+};
 
-router.post('/', async (ctx) => {
+router.post("/", async ctx => {
     initContextState(ctx);
 
     if (!ctx.request.body) {
@@ -91,11 +115,20 @@ router.post('/', async (ctx) => {
         ctx.status = 400;
         ctx.state.errors = errors;
         ctx.state.request = request;
-        return ctx.render('index.ejs');
+        return ctx.render("index.ejs");
     }
 
     try {
-        await insertOne({ client: ctx.state.db, data: request });
+        const newRequest = await insertOne({
+            client: ctx.state.db,
+            data: request
+        });
+        sendRequestConfirmation({
+            emailTo: newRequest.contactEmail,
+            nameTo: newRequest.contactName,
+            maskRequestId: newRequest.id,
+            publicNumber: `${newRequest.publicNumber}`.padStart(5, "0")
+        });
         ctx.state.success = true;
     } catch (error) {
         signale.error(error);
@@ -104,25 +137,29 @@ router.post('/', async (ctx) => {
         ctx.state.request = request;
     }
 
-    return ctx.render('index.ejs');
-})
+    return ctx.render("index.ejs");
+});
 
 const requestStatuses = [
-    'DISPATCH_TODO', 'DISPATCH_PENDING', 'MANAGEMENT_TODO', 'MANAGEMENT_BUILDING', 'MANAGEMENT_BUILT', 'MANAGEMENT_DELIVERED',
+    "DISPATCH_TODO",
+    "DISPATCH_PENDING",
+    "MANAGEMENT_TODO",
+    "MANAGEMENT_BUILDING",
+    "MANAGEMENT_BUILT",
+    "MANAGEMENT_DELIVERED"
 ];
 
-router.get('/tracking/:id', async (ctx) => {
-    const request = await getOne({ client : ctx.state.db, id :ctx.params.id });
+router.get("/track/:id", async ctx => {
+    const request = await getOne({ client: ctx.state.db, id: ctx.params.id });
 
-    ctx.state.commandNumber = `${request.publicNumber}`.padStart(5, '0');
+    ctx.state.commandNumber = `${request.publicNumber}`.padStart(5, "0");
     ctx.state.request = request;
     ctx.state.statusIndex = requestStatuses.indexOf(request.status);
 
-    return ctx.render('tracking.ejs');
+    return ctx.render("tracking.ejs");
 });
 
-app
-    .use(views(path.resolve(__dirname, './views'), { extension: 'ejs' }))
+app.use(views(path.resolve(__dirname, "./views"), { extension: "ejs" }))
     .use(router.routes())
     .use(router.allowedMethods());
 
