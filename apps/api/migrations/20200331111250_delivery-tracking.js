3b537a207788bfec49335edf2ff84eaccef8e216
Alexis Janvier <contact@alexisjanvier.net>
2020-04-01 12:49:22 +0200

    Fix migration

--- apps/api/migrations/20200331111250_delivery-tracking.js
+++ apps/api/migrations/20200331111250_delivery-tracking.js
@@ -5,26 +5,12 @@ exports.up = async function (knex) {
             WHERE status = 'MANAGEMENT_BUILT'
         ;
     `);
-    await knex.raw(`
-            DELETE FROM pg_enum
-            WHERE enumlabel = 'MANAGEMENT_BUILT'
-            AND enumtypid = (
-                SELECT oid FROM pg_type WHERE typname = 'status'
-            )
-        ;
-    `);
     return knex.schema.alterTable('request', (table) => {
         table.jsonb('delivery_tracking').nullable();
     });
 };
 
-exports.down = async function (knex) {
-    await knex.raw(`
-            ALTER TYPE status
-            ADD VALUE 'MANAGEMENT_BUILT'
-            AFTER 'MANAGEMENT_BUILDING'
-        ;
-    `);
+exports.down = function (knex) {
     return knex.schema.alterTable('request', (table) => {
         table.dropColumn('delivery_tracking');
     });
