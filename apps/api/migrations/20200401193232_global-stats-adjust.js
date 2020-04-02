9af9649b646b88f178a0d19f5d07ac30792d11df
tibi <tberriot@gmail.com>
2020-04-02 15:11:12 +0200

    fix view

--- apps/api/migrations/20200401193232_global-stats-adjust.js
+++ apps/api/migrations/20200401193232_global-stats-adjust.js
@@ -6,11 +6,11 @@ exports.up = async function (knex) {
                 count(distinct request.id) as request_nb,
                 count(distinct request.requester_professional_identifier) as requester_nb,
                 sum(request.mask_small_size_quantity + request.mask_large_size_quantity) as requested_shade_qty,
-                sum(case when request.status = 'MANAGEMENT_BUILDING' then request.mask_small_size_quantity + request.mask_large_size_quantity else 0 end) as in_production_shade_qty,
-                sum(request.mask_small_size_delivered_quantity + request.mask_large_size_delivered_quantity) as delivered_shade_qty,
-                sum(case when request.status <> 'DISPATCH_REJECTED' then request.mask_small_size_quantity + request.mask_large_size_quantity else 0 end)
+                sum(case when request.status = 'MANAGEMENT_BUILDING' then request.mask_small_size_quantity + request.mask_large_size_quantity else 0 end)
                 -
-                sum(request.mask_small_size_delivered_quantity + request.mask_large_size_delivered_quantity) as requested_shade_except_rejected_qty
+                sum(request.mask_small_size_delivered_quantity + request.mask_large_size_delivered_quantity) as in_production_shade_qty,
+                sum(request.mask_small_size_delivered_quantity + request.mask_large_size_delivered_quantity) as delivered_shade_qty,
+                sum(case when request.status <> 'DISPATCH_REJECTED' then request.mask_small_size_quantity + request.mask_large_size_quantity else 0 end) as requested_shade_except_rejected_qty
             FROM request
         ;
     `);
