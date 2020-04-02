b232e6344314080b4ed1e4ee61fddf98b8b55f76
Gaël Reyrol <gael@dansmaculotte.fr>
2020-04-02 21:42:31 +0200

    feat: add eslint cypress plugin

--- apps/api/cypress/integration/index.spec.js
+++ apps/api/cypress/integration/index.spec.js
@@ -7,9 +7,23 @@ describe('Index page', function () {
         cy.title().should('contain', 'Usine Partagée');
     });
 
-    it('it contains an organisation block with inputs', function () {
-        cy.get('h4').should('contain', 'ORGANISATION');
+    it('it contains each block titles', function () {
+        const blockTitles = [
+            'ORGANISATION',
+            'CONTACT OPERATIONNEL',
+            'ADRESSE DE LIVRAISON',
+            'PREVISIONNEL',
+            'COMMENTAIRE'
+        ];
 
+        return cy.get('h4').then((titles) => {
+            blockTitles.forEach((blockTitle) => {
+                expect(titles).contain(blockTitle);
+            });
+        });
+    });
+
+    it('it contains an organisation block with inputs', function () {
         cy.get('input[name=requester_name]').should('exist');
         cy.get('input[name=requester_type]').should('exist');
         cy.get('input[name=requester_professional_identifier]').should('exist');
@@ -20,31 +34,23 @@ describe('Index page', function () {
     });
 
     it('it contains an operation contact block with inputs', function () {
-        cy.get('h4').should('contain', 'CONTACT OPERATIONNEL');
-
         cy.get('input[name=contact_name]').should('exist');
         cy.get('input[name=contact_email]').should('exist');
         cy.get('input[name=contact_phone]').should('exist');
     });
 
     it('it contains a delivery address block with inputs', function () {
-        cy.get('h4').should('contain', 'ADRESSE DE LIVRAISON');
-
         cy.get('input[name=delivery_address]').should('exist');
         cy.get('input[name=delivery_postal_code]').should('exist');
         cy.get('input[name=delivery_city]').should('exist');
     });
 
     it('it contains a prevision block with inputs', function () {
-        cy.get('h4').should('contain', 'PREVISIONNEL');
-
         cy.get('input[name=forecast_quantity]').should('exist');
         cy.get('input[name=forecast_days]').should('exist');
     });
 
     it('it contains a comment block with textarea', function () {
-        cy.get('h4').should('contain', 'COMMENTAIRE');
-
         cy.get('textarea[name=requester_comment]').should('exist');
     });
 });
