121252fabede41e8d3b53a256b01ce1591086334
Alexis Janvier <contact@alexisjanvier.net>
2020-04-03 18:28:14 +0200

    Fix deploy

--- apps/api/migrations/20200401243819_customer-cancel.js
+++ apps/api/migrations/20200401243819_customer-cancel.js
@@ -1,7 +1,9 @@
 exports.up = function (knex) {
-    return knex.schema.raw(
-        `ALTER TYPE request_status_type ADD VALUE 'CUSTOMER_CANCELED';`
-    );
+    // This migration is disabled to fix migration problem on Clever Cloud
+    // return knex.schema.raw(
+    //     `ALTER TYPE request_status_type ADD VALUE 'CUSTOMER_CANCELED';`
+    // );
+    return true;
 };
 
 exports.down = function () {
